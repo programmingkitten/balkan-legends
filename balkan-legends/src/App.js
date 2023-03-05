1c1ced9c70c81d34edfc0626ac4596151555cd16
Ivan <vankata294n4@gmail.com>
2023-03-05 19:08:48 +0200

    intial

---
+++ balkan-legends/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import CarController from './components/car-controll/car-control';
+function App() {
+  return (
+    <div>
+      <CarController></CarController>
+    </div>
+  );
+}
+
+export default App;
---
+++ balkan-legends/src/components/car-controll/car-control.js
@@ -0,0 +1,6 @@
+
+import React from "react";
+
+export default function CarController() {
+    return <div><h1>car controll</h1></div>
+}
