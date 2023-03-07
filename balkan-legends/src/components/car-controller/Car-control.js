5ffd4bf1dbd3ae80dc1356d207cfcf75cd5a2372
Ivan <vankata294n4@gmail.com>
2023-03-07 11:51:01 +0200

    removing junk code

--- balkan-legends/src/components/car-controller/Car-control.js
+++ balkan-legends/src/components/car-controller/Car-control.js
@@ -14,15 +14,13 @@ export default function CarController() {
     }
 
     
-    const [revLimiter, setRevLimiter] = useState(false)
-    const [speedHisotry, setSpeedHistory] = useState([]);
     const [gear, setGear] = useState(1)
     const [count, setCount] = useState(0)
     const [gasPower, setGasPower] = useState(0)
     const [speed, setSpeed] = useState(2)
-    const [horsePower, setHorsePower] = useState(500);
+    const horsePower = 500;
+    const maxRpm = 7000;
     const [rpm, setRpm] = useState(800);
-    const [maxRpm, setMaxRpm] = useState(7000)
 
     const [gearRatios, setGearRatios] = useState({
         0: 1,
@@ -54,7 +52,6 @@ export default function CarController() {
         const closest = keys.reduce((a, b) => {
             return Math.abs(b - number) < Math.abs(a - number) ? b : a;
         });
-        console.log(closest)
         return closest
     }
 
@@ -72,14 +69,12 @@ export default function CarController() {
         /windResistenceRations[nearestInAnArray(Object.keys(windResistenceRations),speed)] 
 
         const result = (rpm + bonus)
-        console.log(bonus)
         return result
     }
 
     function gearUpHandler() {
         if (gear < 6) {
             setGear(gear + 1)
-            // 4000, 150, 
             const rpmChange = gearUpRpmCalculator(maxRpm, gearRatios, gear, speed)
             setRpm(rpmChange)
             setGasPower(1)
@@ -98,26 +93,15 @@ export default function CarController() {
             else if (gasPower==0 && rpm > 900) {
                 setRpm(rpm-50)
             }
-            
-            
-            setSpeedHistory(oldArray => [...oldArray, speed])
-            if (speedHisotry.length > 2) {
-                if (speedHisotry.slice(-2)+20 > speedHisotry.slice(-1)) {
             setSpeed(Math.trunc((gearRatios[gear]/maxRpm) * rpm))
-
-                }
-            }
-
         }, 50);
   
-        //Clearing the interval
         return () => clearInterval(interval);
     }, [count]);
    
     
 
     return <div>
-          <h1 className={styles.ok}>OK</h1>
         <div className={styles.game}>
         <label className={rpm < 5000 ? styles.lowSpeed : styles.blinktext}>RPM:{rpm.toFixed(0)}</label><br></br>
         <progress value={rpm} max={maxRpm}></progress>
@@ -127,7 +111,6 @@ export default function CarController() {
         <h1>speed: {speed} km/h</h1>
 
         <div className={`${styles.road} ${styles[__calculateWheelAnimationSpeed()]}`}>
-            <div className={styles.road2}></div>
         </div>
 
 
